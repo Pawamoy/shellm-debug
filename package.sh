1b5f4985c9501d5e501f632c60902627ded9eed0
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-07-02 20:12:41 +0200

    Update package bin

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
-BINS="bin/debug"  #:bin/other-script
+BINS="bin/shellm-debug"  #:bin/other-script
 # LIBS="lib/debug.sh"
 DEPS=gitlab.com/shellm/shellman  #:user2/repo2
 BASH_COMPLETIONS="cmp/debug.completion.bash"
