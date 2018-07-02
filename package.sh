e82b79499f72bb2a77aa56038ebf5afeb7dab966
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-07-02 20:35:37 +0200

    Fix package completions

--- package.sh
+++ package.sh
@@ -1,5 +1,5 @@
 BINS="bin/shellm-debug"  #:bin/other-script
 # LIBS="lib/debug.sh"
 DEPS=gitlab.com/shellm/shellman  #:user2/repo2
-BASH_COMPLETIONS="cmp/debug.completion.bash"
-ZSH_COMPLETIONS="cmp/debug.completion.zsh"
+# BASH_COMPLETIONS="cmp/shellm-debug.completion.bash"
+# ZSH_COMPLETIONS="cmp/shellm-debug.completion.zsh"
