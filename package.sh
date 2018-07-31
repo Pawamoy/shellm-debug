6db89ce84faea5f6c2b0f58500aca7400e4b645f
Timothée Mazzucotelli <timothee.mazzucotelli@protonmail.com>
2018-07-31 20:19:14 +0200

    Fix package.sh

--- package.sh
+++ package.sh
@@ -1,5 +1,4 @@
-BINS="bin/shellm-debug"  #:bin/other-script
-# LIBS="lib/debug.sh"
-DEPS=gitlab.com/shellm/shellman  #:user2/repo2
+BINS="bin/shellm-debug"
+DEPS="gitlab.com/shellm/core"
 # BASH_COMPLETIONS="cmp/shellm-debug.completion.bash"
 # ZSH_COMPLETIONS="cmp/shellm-debug.completion.zsh"
